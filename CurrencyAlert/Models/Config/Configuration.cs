785a04634f5490846b2b667218eee31d1791cb32
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-10 21:57:27 -0700

    Remove unused config options

--- CurrencyAlert/Models/Config/Configuration.cs
+++ CurrencyAlert/Models/Config/Configuration.cs
@@ -19,9 +19,8 @@ public class Configuration : IPluginConfiguration
     public bool OverlayText = true;
     public bool OverlayLongText = true;
     public bool OverlayIcon = true;
-    public bool RightAlign = false;
-    public bool GrowUp = false;
     public bool ShowBackground = false;
+    public bool SingleLine = false;
     public Vector4 OverlayTextColor = KnownColor.White.Vector();
     public Vector4 BackgroundColor = KnownColor.Black.Vector().Fade(0.75f);
     public Vector2 OverlayDrawPosition = new(1920.0f / 2.0f, 1024.0f / 2.0f);
