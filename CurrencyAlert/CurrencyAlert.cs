20ca68aada6f3da0f9fb9234aea5da1870744915
MidoriKami <mi_kami@outlook.com>
2024-07-18 08:32:12 -0700

    Fix dumbass autoformat

--- CurrencyAlert/CurrencyAlert.cs
+++ CurrencyAlert/CurrencyAlert.cs
@@ -63,62 +63,29 @@ public sealed class CurrencyAlertPlugin : IDalamudPlugin {
     }
 
     private static List<TrackedCurrency> GenerateInitialList() => [
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 20, Threshold = 75000, Enabled = true,
-        }, // StormSeal
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 21, Threshold = 75000, Enabled = true,
-        }, // SerpentSeal
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 22, Threshold = 75000, Enabled = true,
-        }, // FlameSeal
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 20, Threshold = 75000, Enabled = true, }, // StormSeal
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 21, Threshold = 75000, Enabled = true, }, // SerpentSeal
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 22, Threshold = 75000, Enabled = true, }, // FlameSeal
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 25, Threshold = 18000, Enabled = true,
-        }, // WolfMarks
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 36656, Threshold = 18000, Enabled = true,
-        }, // TrophyCrystals
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 25, Threshold = 18000, Enabled = true, }, // WolfMarks
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 36656, Threshold = 18000, Enabled = true, }, // TrophyCrystals
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 27, Threshold = 3500, Enabled = true,
-        }, // AlliedSeals
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 10307, Threshold = 3500, Enabled = true,
-        }, // CenturioSeals
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 26533, Threshold = 3500, Enabled = true,
-        }, // SackOfNuts
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 27, Threshold = 3500, Enabled = true, }, // AlliedSeals
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 10307, Threshold = 3500, Enabled = true, }, // CenturioSeals
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 26533, Threshold = 3500, Enabled = true, }, // SackOfNuts
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 26807, Threshold = 800, Enabled = true,
-        }, // BicolorGemstones
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 26807, Threshold = 800, Enabled = true, }, // BicolorGemstones
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 28, Threshold = 1400, Enabled = true,
-        }, // Poetics
-        new TrackedCurrency {
-            Type = CurrencyType.NonLimitedTomestone, Threshold = 1400, Enabled = true,
-        }, // NonLimitedTomestone
-        new TrackedCurrency {
-            Type = CurrencyType.LimitedTomestone, Threshold = 1400, Enabled = true,
-        }, // LimitedTomestone
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 28, Threshold = 1400, Enabled = true, }, // Poetics
+        new TrackedCurrency { Type = CurrencyType.NonLimitedTomestone, Threshold = 1400, Enabled = true, }, // NonLimitedTomestone
+        new TrackedCurrency { Type = CurrencyType.LimitedTomestone, Threshold = 1400, Enabled = true, }, // LimitedTomestone
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 25199, Threshold = 3500, Enabled = true,
-        }, // WhiteCrafterScripts
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 25200, Threshold = 3500, Enabled = true,
-        }, // WhiteGathererScripts
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 33913, Threshold = 3500, Enabled = true,
-        }, // PurpleCrafterScripts
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 33914, Threshold = 3500, Enabled = true,
-        }, // PurpleGathererScripts
+        // Removed for now, todo: replace with dynamic item lookup
+        // new TrackedCurrency { Type = CurrencyType.Item, ItemId = 25199, Threshold = 3500, Enabled = true, }, // WhiteCrafterScripts
+        // new TrackedCurrency { Type = CurrencyType.Item, ItemId = 25200, Threshold = 3500, Enabled = true, }, // WhiteGathererScripts
+        // new TrackedCurrency { Type = CurrencyType.Item, ItemId = 33913, Threshold = 3500, Enabled = true, }, // PurpleCrafterScripts
+        // new TrackedCurrency { Type = CurrencyType.Item, ItemId = 33914, Threshold = 3500, Enabled = true, }, // PurpleGathererScripts
 
-        new TrackedCurrency {
-            Type = CurrencyType.Item, ItemId = 28063, Threshold = 7500, Enabled = true,
-        },
+        new TrackedCurrency { Type = CurrencyType.Item, ItemId = 28063, Threshold = 7500, Enabled = true, }, // Skybuilders scripts
     ];
 }
