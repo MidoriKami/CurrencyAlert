c7bbc874122c26c8f3d748bef79420304c90643c
MidoriKami <mi_kami@outlook.com>
2024-08-29 21:50:28 -0700

    Remove node flags from container
    closes #22

--- CurrencyAlert/Classes/OverlayController.cs
+++ CurrencyAlert/Classes/OverlayController.cs
@@ -78,7 +78,6 @@ public unsafe class OverlayController() : NativeUiOverlayController(Service.Addo
         if (overlayListNode is null) return;
 
         overlayListNode.SetStyle(System.Config.ListStyle);
-        overlayListNode.NodeFlags |= NodeFlags.EmitsEvents | NodeFlags.HasCollision | NodeFlags.RespondToMouse;
 
         foreach (var node in overlayListNode) {
             node.Refresh();
